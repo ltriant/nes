a55aad06259d729cf4bf196360de6184a43c096c
Luke Triantafyllidis <ltriant@cpan.org>
2018-08-29 10:23:24 +1000

    ppu: addr: implement PPUADDR register
    Again, as before, it's a barebones implementation that may be missing
    things... I won't know until I get stuff starting to work.

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -3,12 +3,14 @@ mod mask;
 mod status;
 mod oam;
 mod scroll;
+mod addr;
 
 use mem::Memory;
 use ppu::ctrl::PPUCtrl;
 use ppu::mask::PPUMask;
 use ppu::status::PPUStatus;
 use ppu::scroll::PPUScroll;
+use ppu::addr::PPUAddr;
 use ppu::oam::OAM;
 
 pub struct PPU {
@@ -18,6 +20,7 @@ pub struct PPU {
     oam_addr: u8,
     oam: OAM,
     scroll: PPUScroll,
+    ppu_addr: PPUAddr,
 }
 
 impl Memory for PPU {
@@ -41,6 +44,7 @@ impl Memory for PPU {
             0x2003 => Ok(0), // OAMADDR is write-only
             0x2004 => panic!("OAMData is unreadable... I think. Double check if this panic happens."),
             0x2005 => panic!("PPUScroll is unreadable... I think"),
+            0x2006 => panic!("PPUAddr is unreadable... I think"),
             _ => panic!("bad PPU address 0x{:04X}", address)
         }
     }
@@ -72,6 +76,10 @@ impl Memory for PPU {
                 self.scroll.write(val);
                 Ok(val)
             },
+            0x2006 => {
+                self.ppu_addr.write(val);
+                Ok(val)
+            },
             _ => panic!("bad PPU address 0x{:04X}", address)
         }
     }
@@ -86,6 +94,7 @@ impl PPU {
             oam: OAM::new_nes_oam(),
             oam_addr: 0,
             scroll: PPUScroll::new_ppu_scroll(),
+            ppu_addr: PPUAddr::new_ppu_addr(),
         }
     }
 
