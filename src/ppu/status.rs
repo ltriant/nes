b41b3eceb05f18d22e6a95dcdfd6cc9d10a006eb
Luke Triantafyllidis <ltriant@cpan.org>
2019-02-16 22:48:43 +1100

    ppu: status: add methods to set the PPU status register
    The PPU needs to manipulate the PPU status register at various times.
    It's not set by writing to particular memory locations, but when
    particular events are triggered.

--- src/ppu/status.rs
+++ src/ppu/status.rs
@@ -6,14 +6,47 @@ impl PPUStatus {
         (val & 0x80) != 0
     }
 
+    pub fn set_vblank_started(&mut self, val: bool) {
+        let PPUStatus(old) = *self;
+
+        if val {
+            *self = PPUStatus(old | 0x80);
+        }
+        else {
+            *self = PPUStatus(old & !0x80);
+        }
+    }
+
     pub fn sprite_zero_hit(&self) -> bool {
         let &PPUStatus(val) = self;
         (val & 0x40) != 0
     }
 
+    pub fn set_sprite_zero_hit(&mut self, val: bool) {
+        let PPUStatus(old) = *self;
+
+        if val {
+            *self = PPUStatus(old | 0x40);
+        }
+        else {
+            *self = PPUStatus(old & !0x40);
+        }
+    }
+
     pub fn sprite_overflow(&self) -> bool {
         let &PPUStatus(val) = self;
         (val & 0x20) != 0
+    }
+
+    pub fn set_sprite_overflow(&mut self, val: bool) {
+        let PPUStatus(old) = *self;
+
+        if val {
+            *self = PPUStatus(old | 0x20);
+        }
+        else {
+            *self = PPUStatus(old & !0x20);
+        }
     }
 }
 
@@ -26,7 +59,12 @@ mod tests {
         let status = PPUStatus(0b10000000);
         assert!(status.vblank_started());
 
-        // TODO how to set vblank status?
+        let mut status = PPUStatus(0x00);
+        assert!(!status.vblank_started());
+        status.set_vblank_started(true);
+        assert!(status.vblank_started());
+        status.set_vblank_started(false);
+        assert!(!status.vblank_started());
     }
 
     #[test]
@@ -34,7 +72,12 @@ mod tests {
         let status = PPUStatus(0b01000000);
         assert!(status.sprite_zero_hit());
 
-        // TODO how to set sprite zero hit?
+        let mut status = PPUStatus(0x00);
+        assert!(!status.sprite_zero_hit());
+        status.set_sprite_zero_hit(true);
+        assert!(status.sprite_zero_hit());
+        status.set_sprite_zero_hit(false);
+        assert!(!status.sprite_zero_hit());
     }
 
     #[test]
@@ -42,6 +85,11 @@ mod tests {
         let status = PPUStatus(0b00100000);
         assert!(status.sprite_overflow());
 
-        // TODO how to set sprite overflow?
+        let mut status = PPUStatus(0x00);
+        assert!(!status.sprite_overflow());
+        status.set_sprite_overflow(true);
+        assert!(status.sprite_overflow());
+        status.set_sprite_overflow(false);
+        assert!(!status.sprite_overflow());
     }
 }
