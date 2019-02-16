a5f5ec80a5533aaa5c9c2888161e2d2bf5d1d300
Luke Triantafyllidis <ltriant@cpan.org>
2019-02-16 22:03:41 +1100

    ppu: status: unit tests
    The API needs some additions, so adding some tests in first...

--- src/ppu/status.rs
+++ src/ppu/status.rs
@@ -16,3 +16,32 @@ impl PPUStatus {
         (val & 0x20) != 0
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_vblank() {
+        let status = PPUStatus(0b10000000);
+        assert!(status.vblank_started());
+
+        // TODO how to set vblank status?
+    }
+
+    #[test]
+    fn test_sprite_zero() {
+        let status = PPUStatus(0b01000000);
+        assert!(status.sprite_zero_hit());
+
+        // TODO how to set sprite zero hit?
+    }
+
+    #[test]
+    fn test_sprite_overflow() {
+        let status = PPUStatus(0b00100000);
+        assert!(status.sprite_overflow());
+
+        // TODO how to set sprite overflow?
+    }
+}
