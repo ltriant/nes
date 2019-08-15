622d3b441931b97cece0b3d37945f479fd912fbb
Luke Triantafyllidis <ltriant@cpan.org>
2019-08-15 10:11:03 +1000

    main: accept parameters on argv for other roms
    Defaults to the nestest rom, but I want the ability to pick others
    now...

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,17 @@ mod ines;
 mod addr;
 mod ppu;
 
+use std::env;
+
 use crate::console::Console;
 
 fn main() {
     env_logger::init();
 
+    let rom = env::args().skip(1).next()
+        .unwrap_or(String::from("roms/nestest.nes"));
+
     let mut console = Console::new_nes_console();
-
-    console.insert_cartridge("roms/nestest.nes")
-        .expect("could not insert nestest");
-    //console.insert_cartridge("roms/instr_test-v5/official_only.nes")
-    //    .expect("could not insert nestest");
-
-
+    console.insert_cartridge(rom).expect("could not insert nestest");
     console.power_up();
 }
