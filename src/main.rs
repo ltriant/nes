a25f06413a919df1f40e1525a7dfe28c5f1d62c5
Luke Triantafyllidis <ltriant@cpan.org>
2020-01-23 10:31:01 +1100

    main: print appropriate error message when no args are specified
    The default behaviour, which was useful very early on for testing, was
    to load roms/nestest.nes if no parameter was specified. This no longer
    makes sense.

--- src/main.rs
+++ src/main.rs
@@ -24,29 +24,31 @@ use crate::ines::CartridgeError;
 fn main() {
     env_logger::init();
 
-    let rom = env::args().skip(1).next()
-        .unwrap_or(String::from("roms/nestest.nes"));
+    if let Some(rom) = env::args().skip(1).next() {
+        let mut console = Console::new_nes_console();
+        if let Err(e) = console.insert_cartridge(&rom) {
+            match e {
+                CartridgeError::IO(io_e) => {
+                    println!("There was an error reading ROM data from {}: {}",
+                             rom, io_e);
+                },
+                CartridgeError::InvalidMagic => {
+                    println!("File {} is invalid. Expected iNES formatted ROM.", rom);
+                },
+                CartridgeError::UnsupportedCartridge => {
+                    println!("Unsupported cartridge type. Only supports NTSC for now.");
+                },
+                CartridgeError::UnsupportedMapper(m) => {
+                    println!("Unsupported mapper type: {}", m);
+                },
+            }
 
-    let mut console = Console::new_nes_console();
-    if let Err(e) = console.insert_cartridge(&rom) {
-        match e {
-            CartridgeError::IO(io_e) => {
-                println!("There was an error reading ROM data from {}: {}",
-                         rom, io_e);
-            },
-            CartridgeError::InvalidMagic => {
-                println!("File {} is invalid. Expected iNES formatted ROM.", rom);
-            },
-            CartridgeError::UnsupportedCartridge => {
-                println!("Unsupported cartridge type. Only supports NTSC for now.");
-            },
-            CartridgeError::UnsupportedMapper(m) => {
-                println!("Unsupported mapper type: {}", m);
-            },
+            process::exit(1);
         }
 
+        console.power_up();
+    } else {
+        println!("Missing required parameter: a path to a ROM file.");
         process::exit(1);
     }
-
-    console.power_up();
 }
